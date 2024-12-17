18dac921474a2aeac1393e055cac11fba714f2ce
aryal05 <aryal.rajat05@gmail.com>
2024-12-17 20:11:57 +0545

    change in line of code in js anmd html

--- Weather-Application/weather.js
+++ Weather-Application/weather.js
@@ -1,2 +1,9 @@
 const apiKey = "173c734987574c9bc327964b5e39b4e0";
-const url = "https://api.openweathermap.org/data/2.5/weather?uints=metric&q=bangalore"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?uints=metric&q=London"
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+    var data = await response.json()
+    console.log(data)
+}
+checkWeather()
