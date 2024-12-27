9eccee264d5f74532b5372b4ee1b5ac478bc0a2b
aryal05 <aryal.rajat05@gmail.com>
2024-12-27 20:08:59 +0545

    weather appliocation after the api link

--- Weather-Application/weather.js
+++ Weather-Application/weather.js
@@ -1,9 +1,21 @@
-const apiKey = "173c734987574c9bc327964b5e39b4e0";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?uints=metric&q=London"
+const apiKey = '30985cd12f34acf0f480fe9bb6ab0c7c';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=germany';
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
-    var data = await response.json()
-    console.log(data)
+async function weather() {
+    try {
+        const response = await fetch(url + `&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        document.getElementById("city").innerHTML = data.name;
+        document.getElementById("cityName").innerHTML = data.name;
+        document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "&degc";
+        document.getElementById("wind").innerHTML = data.wind.speed + "km/h";
+    } catch (error) {
+        alert("City not found or another error occurred.");
+        console.error(error);
+    }
 }
-checkWeather()
+weather()
