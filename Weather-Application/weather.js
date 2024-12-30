7c19807846708d378642bdfbe6da8fd7f95b0d58
aryal05 <aryal.rajat05@gmail.com>
2024-12-30 20:19:58 +0545

    weather icon called from api

--- Weather-Application/weather.js
+++ Weather-Application/weather.js
@@ -2,9 +2,7 @@ const apiKey = '30985cd12f34acf0f480fe9bb6ab0c7c';
 const url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 const searchBox = document.getElementById('search-box');
 const searchButton = document.getElementById('btn');
-const weatherIcon = document.getElementsByClassName('weather-icon')[0];
 const weatherDescription = document.getElementById('weather-description');
-
 
 function updateDateTime() {
     const now = new Date();
@@ -34,7 +32,10 @@ async function weather(city) {
 
         const data = await response.json();
         // console.log(data);
-
+        const iconCode = data.weather[0].icon; 
+        const weatherIcon = document.querySelector('.weather-icon'); 
+        weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+        weatherIcon.alt = data.weather[0].description;
         
         document.getElementById("city").innerHTML = `${data.name} City:`;
         // document.getElementById("bold").innerHTML = data.name;
@@ -48,30 +49,22 @@ async function weather(city) {
         weatherDescription.innerHTML = data.weather[0].description;
 
         // Update weather icon
-        if (data.weather[0].main === "Clouds") {
-            weatherIcon.src = "images/clouds.png";
-            document.body.style.backgroundImage = "url('images/cloudsbg.jpg')";
+        if (data.weather[0].main === "Clouds"){ 
+            document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1509803874385-db7c23652552?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
         } else if (data.weather[0].main === "Rain") {
-            weatherIcon.src = "images/heavy-rain.png";
-            document.body.style.backgroundImage = "url('images/rainbg.jpg')";
+            document.body.style.backgroundImage = "url('https://st4.depositphotos.com/4876263/20564/i/450/depositphotos_205644092-stock-photo-heavy-rain-falls-road-which.jpg')";
         } else if (data.weather[0].main === "Mist") {
-            weatherIcon.src = "images/mist.png";
-            document.body.style.backgroundImage = "url('images/mistbg.jpg')";
+            document.body.style.backgroundImage = "url('https://plus.unsplash.com/premium_photo-1669613233573-4911a0a81c63?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm9yZXN0JTIwbWlzdHxlbnwwfHwwfHx8MA%3D%3D')";
         } else if (data.weather[0].main === "Drizzle") {
-            weatherIcon.src = "images/drizzle.png";
-            // document.body.style.backgroundImage = "url('images/drizzlebg.jpg')";
+            document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMW52awlZheuRgxcetXz2igHvStkdd3IPLxA&s')";
         } else if (data.weather[0].main === "Haze") {
-            weatherIcon.src = "images/fog.png";
-            document.body.style.backgroundImage = "url('images/fogbg.jpg')";
+            document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9Y_wFgqTOto5tLeyZFnak8FwpCiTs0sLZqg&s')";
         } else if (data.weather[0].main === "Clear") {
-            weatherIcon.src = "images/clear-sky.png";
-            document.body.style.backgroundImage = "url('images/clearbg.jpg')";
+            document.body.style.backgroundImage = "url('https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bright-sun-and-clouds.jpg?s=612x612&w=0&k=20&c=MGd2-v42lNF7Ie6TtsYoKnohdCfOPFSPQt5XOz4uOy4=')";
         } else if (data.weather[0].main === "Snow") {
-            weatherIcon.src = "images/snow.png";
-            document.body.style.backgroundImage = "url('images/snowbg.jpg')";
-        } else {
-            weatherIcon.src = "images/default.png"; // Fallback
-            document.body.style.backgroundImage = "url('images/defaultbg.jpg')";
+            document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1707688658001-4248125102ef?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c25vdyUyMGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D')";
+        } else { 
+            document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9Y_wFgqTOto5tLeyZFnak8FwpCiTs0sLZqg&s')";
         }
         
     } catch (error) {
